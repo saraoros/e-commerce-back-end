949ef4cf89b2c61b8fabe21bfdfc68f7727cae9a
Sara Gochez <saragochez13@gmail.com>
2022-03-25 15:45:30 -0700

    ProductTag and Tag models completed

--- models/Tag.js
+++ models/Tag.js
@@ -4,31 +4,18 @@ const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
-// Tag
-
-// id
-
-// Integer
-
-// Doesn't allow null values
-
-// Set as primary key
-
-// Uses auto increment
-
-// tag_name
-
-// String
-
-
-
-
-
-
-
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
