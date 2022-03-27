20c6d02256e33059d3cc12da00d47aeaa9efcb08
Sara Gochez <saragochez13@gmail.com>
2022-03-27 16:54:57 -0700

    final touches to routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,9 +9,6 @@ const { findOne, findAll } = require('../../models');
 
 router.get('/', (req, res) => {
   Product.findAll({
-
-    // should I put attributes inside include array?
-
     attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
     include: [
       {
@@ -39,35 +36,28 @@ router.get('/:id', (req, res) => {
       id: req.params.id,
     },
     attriubutes: ['id', 'product_name', 'price', 'stock'],
+
     include: [
       {
         model: Category,
         attributes: ['id', 'category_name'],
-      },
-      {
-        model: Tag,
-        attributes: ['id', 'tag_name'],
+        // include: {
+        //   model: Tag,
+        //   attributes: ['id', 'tag_name'],
+        // },
       },
     ],
   }).then((dbProductData) => {
     if (!dbProductData) {
       res
         .status(400)
-        .json({ message: 'Sorry! No post was found with this ID!' });
+        .json({ message: 'Sorry! No product was found with this ID!' });
     }
   });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   Product.create(req.body)({
     product_name: req.body.product_name,
     price: req.body.price,
@@ -89,19 +79,16 @@ router.post('/', (req, res) => {
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      // res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
-      console.log(err);
+      //console.log(err);
       res.status(400).json(err);
     });
 });
 
 // update product
-
-// should I do anything in this route??
-
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -113,29 +100,29 @@ router.put('/:id', (req, res) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+    // .then((productTags) => {
+    //   // get list of current tag_ids
+    //   const productTagIds = productTags.map(({ tag_id }) => tag_id);
+    //   // create filtered list of new tag_ids
+    //   const newProductTags = req.body.tagIds
+    //     .filter((tag_id) => !productTagIds.includes(tag_id))
+    //     .map((tag_id) => {
+    //       return {
+    //         product_id: req.params.id,
+    //         tag_id,
+    //       };
+    //     });
+    //   // figure out which ones to remove
+    //   const productTagsToRemove = productTags
+    //     .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+    //     .map(({ id }) => id);
 
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
+    //   // run both actions
+    //   return Promise.all([
+    //     ProductTag.destroy({ where: { id: productTagsToRemove } }),
+    //     ProductTag.bulkCreate(newProductTags),
+    //   ]);
+    // })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
